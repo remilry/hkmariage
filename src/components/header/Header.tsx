02ae34dcf1781e68891f58d834e57ab2881bf36c
Rémi Leroy <remil@bam.tech>
2024-09-08 21:22:27 +0200

    init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <section>
+      <a href="">Acceuil</a>
+      <a href="">Infos</a>
+      <a href="">Logements</a>
+      <a href="">Photos</a>
+      <a href="">Contacts</a>
+    </section>
+  );
+};
