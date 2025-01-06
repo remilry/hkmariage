69bb8c37ccdc532834f63d80bd71d01fb918e643
Rémi Leroy <remil@bam.tech>
2025-01-06 20:47:28 +0100

    add infos to hostingpage

--- src/modules/hosting/HostingPage.tsx
+++ src/modules/hosting/HostingPage.tsx
@@ -3,6 +3,7 @@ import BulleImg from "../../assets/bulle.webp";
 import ChambreImg from "../../assets/chambre.webp";
 import FrontDoor from "../../assets/frontDoor.webp";
 import Cabane from "../../assets/cabane.webp";
+import GiteImg from "../../assets/gite.webp";
 
 export const HostingPage = () => {
   return (
@@ -17,8 +18,9 @@ export const HostingPage = () => {
           <div className="textInsideContainer">
             <h2>Les bulles</h2>
             <ul>
-              <li>Nombre de bulles : 4</li>
+              <li>Nombre de bulles : 5</li>
               <li>Nombre de personnes par bulle : 4</li>
+              <li>{"Prix : 30€ / personne (vendredi & samedi)"}</li>
             </ul>
           </div>
         </div>
@@ -31,9 +33,10 @@ export const HostingPage = () => {
           <div className="textInsideContainer">
             <h2>Les chambres</h2>
             <ul>
-              <li>Nombre de chambres : XX</li>
+              <li>Nombre de chambres : 4</li>
               <li>Nombre de personnes par chambre : 4 à 6</li>
-              <li>Possibilité de rajouter des matelas</li>
+              <li>{"Prix : 30€ / personne (vendredi & samedi)"}</li>
+              <li>Matelas en plus gratuit</li>
             </ul>
           </div>
         </div>
@@ -42,24 +45,42 @@ export const HostingPage = () => {
       <div className="slice imageRight">
         <div className="textContainer">
           <div className="textInsideContainer">
-            <h2>Tente et caravanne</h2>
-            <span>
-              Un terrain est prévu pour dormir en tente ou en caravanne. Il faut
-              cependant ramener le matériel.
-            </span>
+            <h2>Gîte</h2>
+            <ul>
+              <li>Nombre de personnes : 6</li>
+              <li>{"Prix : 30€ / personne (vendredi & samedi)"}</li>
+            </ul>
           </div>
         </div>
-        <img className="imgRight" src={FrontDoor} alt="chambre" />
+        <img className="imgRight" src={GiteImg} alt="chambre" />
       </div>
       <Spacer />
       <div className="slice imageLeft">
-        <img className="imgLeft" src={Cabane} alt="chambre" />
+        <img className="imgRight" src={FrontDoor} alt="chambre" />
+        <div className="textContainer">
+          <div className="textInsideContainer">
+            <h2>Tente et caravane</h2>
+            <span>
+              Un terrain est prévu pour dormir en tente ou en caravanne. Il faut
+              cependant ramener son matériel.
+            </span>
+          </div>
+        </div>
+      </div>
+      <Spacer />
+      <div className="slice imageRight">
         <div className="textContainer">
           <div className="textInsideContainer">
             <h2>Se loger en dehors</h2>
-            <span>Bah demmerde toi !</span>
+            <span>Quelques villes aux alentours : </span>
+            <ul>
+              <li>St Gervazy</li>
+              <li>St Germain Lembron</li>
+              <li>Issoire</li>
+            </ul>
           </div>
         </div>
+        <img className="imgLeft" src={Cabane} alt="chambre" />
       </div>
     </div>
   );
