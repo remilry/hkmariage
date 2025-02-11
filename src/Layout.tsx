ee6145f0c3393a54e65ef5c67a7adc6a6daa1cdf
Rémi Leroy <remil@bam.tech>
2025-02-11 13:47:53 +0100

    add analytics

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/header/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Layout = () => {
   return (
     <>
+      <Analytics />
       <Header />
       <Outlet />
     </>
